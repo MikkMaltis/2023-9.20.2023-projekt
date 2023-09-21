c587e31586a4933ecbf2cdc2f2ba9ffd852cfd66
MikkMaltis <maltismikk@gmail.com>
2023-09-21 21:51:46 +0300

    .

--- src/components/ManualItem.vue
+++ src/components/ManualItem.vue
@@ -1,20 +1,3 @@
 <template>
-    <div class="container">
-        <button class="manual-add">
-        <p>LISA KÄSITSI</p>
-    </button>
-    </div>
-
+    <img src="../assets/pics/type.png" alt="type">
 </template>
-
-<style scoped>
-.manual-add {
-    width: 130px;
-    height: 40px;
-    border-radius: 10px;
-    border: 0px;
-    background-color: #4848484d;
-    color: #000000;
-    font-size: smaller;
-}
-</style>
