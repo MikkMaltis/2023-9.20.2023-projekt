01eeefca6ccd6e82812a82d00bd8a492a7f5e44d
MikkMaltis <maltismikk@gmail.com>
2023-09-21 21:23:50 +0300

    muudatused

--- src/views/PlasticView.vue
+++ src/views/PlasticView.vue
@@ -2,5 +2,68 @@
 </script>
 
 <template>
-    <h1>HI</h1>
+<div class="rinsing-container">
+    <p class="rinsing">
+        Loputa vajadusel kergelt, et ei määriks teisi pakendeid ja kotti. Jäta korgid-kaaned peale.
+    </p>
+    <img class="fits" src="../assets/pics/sobibTEST.png" alt="sobibTEST">
+    <ul class="plastic-info">
+        <li>
+            Kilekotid ja pakkekile
+        </li>
+        <li>
+            Penoplast
+        </li>
+        <li>
+            Kosmeetikatooted
+        </li>
+        <li>
+            Leiva ja saia kotid
+        </li>
+        <li>
+            Salatikarbid
+        </li>
+        <li>
+            Konservikarbid
+        </li>
+        <li>
+            Alumiiniumpurgid
+        </li>
+        <li>
+            Metallkorgid ja -kaaned
+        </li>
+        <li>
+            Piima-, mahla- ja veinitooted
+        </li>
+    </ul>
+</div>
+
+
 </template>
+
+<style scoped>
+
+.rinsing-container {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    width: 60%;
+    text-align: center;
+}
+
+.rinsing {
+    font-size: 1.6rem;
+
+}
+
+.fits {
+    height: 200px;
+}
+
+.plastic-info {
+    font-size: 1.4rem;
+    line-height: 35px;
+
+}
+
+</style>
