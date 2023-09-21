8c71939ecc60838cc5b0be26a780c6df48b9dbf9
Tene Tiitma <tenetiitma@gmail.com>
2023-09-21 17:10:08 +0300

    Homepage

---
+++ src/components/SearchItem.vue
@@ -0,0 +1,3 @@
+<template>
+<img src="../assets/pics/Search.png" alt="search">
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,42 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import InfoItem from '../components/InfoItem.vue';
+import SearchItem from '../components/SearchItem.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <img src="../assets/pics/logo.png" class="logo" alt="logo">
+    <div>
+      ascinpaefijpacmjoadovadv
+    </div>
+    <div class="footer">
+      <div class="position">
+        <SearchItem />
+        <InfoItem />
+    </div>
+      </div>
+    
   </main>
 </template>
+
+<style scoped>
+.logo {
+  max-height: 100%;
+  max-width: 100%;
+}
+
+
+
+.footer {
+  position: absolute;
+  bottom: 50px;
+  width: 100%;
+}
+
+
+.position {
+  display: flex;
+  justify-content: space-evenly;
+}
+
+</style>
